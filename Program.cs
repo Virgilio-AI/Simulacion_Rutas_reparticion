265b0991058106cb6ddfd2cb0cbf4d621f5d3af8
Virgilio-AI <virgiliomurilloochoa1@gmail.com>
2022-05-13 22:14:47 -0700

    agregada la simulacion para preveer si son suficientes los camiones que van a salir

--- Program.cs
+++ Program.cs
@@ -12,9 +12,17 @@ class Program
 
     public static String[] names = { "tienda1", "tienda2","tienda3","tienda4" };
 
-    public static int[,] pedidos = { { 1, 2 }, { 3, 4 },{ 25, 5 },{ 30, 0 } };
+    public static int[,] pedidos = { { 1, 2 ,1}, { 3, 4 ,2},{ 25, 5,2 },{ 30, 0 ,1} };
 
-    public static String[] productos = { "refresco", "pan" };
+    public static String[] productos = { "refresco", "pan","verdura" };
+
+    public static String[] camiones = { "refresco", "pan", "verdura" };
+
+    public static int[] camionesCapacidad = { 120,270,95};
+
+    public static int totalCamiones = 5;
+    public static int[] unidadesCamiones = { 3, 3, 3 };
+
 
 
 
@@ -68,7 +76,6 @@ class Program
             {
                 Console.Write("\n cuantos productos de " + productos[j] +  " quiere?\n");
                 int ans = int.Parse(Console.ReadLine());
-                Console.Write("i: " + i + "j: " + j);
                 pedidos[idxs[i],j] = ans;
 
             }
@@ -82,7 +89,57 @@ class Program
 
     static void IniciarSimulacion()
     {
-        Console.Write("Iniciando la simulacion");
+        Console.Write("Iniciando la simulacion de productos");
+        // este elemento es para guardar la maxima cantidad de camiones que pueden salir a repartir
+        int tempCamiones = totalCamiones;
+        // este arreglo es para llevar la cantidad que camiones que van a salir de cada tipo
+        int[] camionesCantidad = new int[camiones.Length];
+
+        // este arreglo es para incluir la cantidad de elementos 
+        // en todos los camiones que van a salir a repartir
+        int[] elementosARepartir = new int[camiones.Length];
+
+        for(int i=0;i<camiones.Length;i++)
+        {
+            Console.Write("\nLos Camiones del tipo " + camiones[i] + "\ntienen capacidad para almacenar" + camionesCapacidad[i] + "elementos\n");
+            Console.Write("\ncuantos camiones del tipo: " + camiones[i] + " max( " + totalCamiones + ")?\n");
+            camionesCantidad[i] = int.Parse(Console.ReadLine());
+            totalCamiones -= camionesCantidad[i];
+
+            elementosARepartir[i] = camionesCantidad[i] * camionesCapacidad[i];
+        }
+
+
+        int[] idxs = sortIndeces(pedidos);
+        for (int i = 0; i < names.Length; i++)
+        {
+            //repartir productos
+            Console.Write("\n repartiendo en la tienda " + names[idxs[i]] + "\n");
+            for (int j = 0; j < productos.Length; j++)
+            {
+                elementosARepartir[j] -= pedidos[idxs[i], j];
+                // esta es la traba para seguir obteniendo los camiones
+                while(elementosARepartir[j] < 0)
+                {
+                    Console.Write("\nNo se pudo satisfacer la demanda\n");
+                    Console.Write("\nAgrega otro camion de " + productos[j] + "(yes/no)\n");
+                    string answer = Console.ReadLine();
+                    if(answer == "yes")
+                    {
+                        elementosARepartir[j] += camionesCapacidad[j];
+                    }
+                }
+                Console.Write("\nProducto " + productos[j] + " -- " + pedidos[idxs[i], j] + "\n");
+            }
+            Console.Write(" \n==== Reparticion Finalizada === \n\n");
+            // levantar pedidos
+            Console.Write("\n lavantando pedidos en la tienda" + names[idxs[i]]);
+            for (int j = 0; j < productos.Length; j++)
+            {
+                Console.Write("\n cuantos productos de " + productos[j] + " quiere?\n");
+            }
+            Console.Clear();
+        }
     }
     static void FirstMenu()
     {
