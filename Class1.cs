5a3abbdf6db3fa0440a15ae6cb6005de2c706dfe
Virgilio-AI <virgiliomurilloochoa1@gmail.com>
2022-05-15 00:52:59 -0500

    bitacora exitosa junto con todos los requerimientos del programa

--- Class1.cs
+++ Class1.cs
@@ -10,6 +10,9 @@ using Newtonsoft.Json.Linq;
 
 public class CLass1
 {
+    public static string bitacora_path = "../../../bitacora.txt";
+    public static string json_path = "../../../data_base.json";
+
     public dynamic names;
     public dynamic numCustomers;
     public dynamic pedidos;
@@ -18,26 +21,28 @@ public class CLass1
     public dynamic camionesCapacidad;
     public dynamic totalCamiones;
     public dynamic unidadesCamiones;
+    // for loading the database
+    public dynamic data_base;
 
     public CLass1()
     {
-        StreamReader r = new StreamReader("../../../data_base.json");
+        StreamReader r = new StreamReader(json_path);
         string json = r.ReadToEnd();
 
         //string o1 = File.ReadAllText("../../../data_base.json");
 
 
-        dynamic hola = JsonConvert.DeserializeObject(json);
+        dynamic data_base = JsonConvert.DeserializeObject(json);
         //Console.Write(hola);
         //Console.Write(hola.names);
-        names = hola.names;
-        numCustomers = hola.numCustomers;
-        pedidos = hola.pedidos;
-        productos = hola.productos;
-        camiones = hola.camiones;
-        camionesCapacidad = hola.camionesCapacidad;
-        totalCamiones = hola.totalCamiones;
-        unidadesCamiones = hola.unidadesCamiones;
+        names = data_base.names;
+        numCustomers = data_base.numCustomers;
+        pedidos = data_base.pedidos;
+        productos = data_base.productos;
+        camiones = data_base.camiones;
+        camionesCapacidad = data_base.camionesCapacidad;
+        totalCamiones = data_base.totalCamiones;
+        unidadesCamiones = data_base.unidadesCamiones;
 
 
 
@@ -54,9 +59,48 @@ public class CLass1
     }
     public void writeChanges()
     {
+        string output = Newtonsoft.Json.JsonConvert.SerializeObject(data_base, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText(json_path, output);
+    }
 
+    public void EscribirEnBitacora(string toWrite)
+    {
+        using (StreamWriter sw = File.AppendText(bitacora_path))
+        {
+            sw.WriteLine(toWrite);
+        }
     }
 
 
+    public void leerBitacora()
+    {
+        String line;
+        try
+        {
+            //Pass the file path and file name to the StreamReader constructor
+            StreamReader sr = new StreamReader(bitacora_path);
+            //Read the first line of text
+            line = sr.ReadLine();
+            //Continue to read until you reach end of file
+            while (line != null)
+            {
+                //write the line to console window
+                Console.WriteLine(line);
+                //Read the next line
+                line = sr.ReadLine();
+            }
+            //close the file
+            sr.Close();
+            Console.ReadLine();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block.");
+        }
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -10,33 +10,37 @@ class Program
 
 
 
-    public static String[] names = { "tienda1", "tienda2","tienda3","tienda4" };
+    public static dynamic names;
 
-    public static int[,] pedidos = { { 1, 2 ,1}, { 3, 4 ,2},{ 25, 5,2 },{ 30, 0 ,1} };
+    public static dynamic pedidos;
 
-    public static String[] productos = { "refresco", "pan","verdura" };
+    public static dynamic productos ;
 
-    public static String[] camiones = { "refresco", "pan", "verdura" };
+    public static dynamic camiones ;
 
-    public static int[] camionesCapacidad = { 120,270,95};
+    public static dynamic camionesCapacidad;
 
-    public static int totalCamiones = 5;
-    public static int[] unidadesCamiones = { 3, 3, 3 };
+    public static int totalCamiones;
+    public static dynamic unidadesCamiones;
+
+    public static dynamic wholeDatabase;
+
+    public static bool databaseLoaded=false;
 
 
 
 
-    static int[] sortIndeces(int[,] pedidos)
+    static int[] sortIndeces(dynamic pedidos)
     {
 
         List<int> list = new List<int>();
 
-        for(int i =0;i<names.Length;i++)
+        for(int i =0;i<names.Count;i++)
         {
             int sumi = 0;
-            for(int j=0;j<productos.Length;j++)
+            for(int j=0;j<productos.Count;j++)
             {
-                sumi += pedidos[i, j];
+                sumi += (int)pedidos[i][j];
             }
             list.Add(sumi);
         }
@@ -59,65 +63,79 @@ class Program
     }
     static void IniciarReparticion()
     {
-        Console.Write("Iniciando reparticion de productos");
+        Console.Write("====== Iniciando reparticion de productos ====== ");
+        Console.Write("===== los camiones salieron a las rutas ======== ");
+        Console.Write("------ se atenderan primero a los que generen mas dinero ---- ");
+
+
         int[] idxs = sortIndeces(pedidos);
-        for (int i = 0; i < names.Length; i++)
+        for (int i = 0; i < names.Count; i++)
         {
             //repartir productos
             Console.Write("\n repartiendo en la tienda " + names[idxs[i]] + "\n");
-            for (int j = 0; j < productos.Length; j++)
+            for (int j = 0; j < productos.Count; j++)
             {
-                Console.Write("\nProducto " + productos[j] + " -- "+ pedidos[idxs[i],j]  + "\n");
+                Console.Write("\nProducto " + productos[j] + " -- "+ pedidos[idxs[i]][j]  + "\n");
             }
             Console.Write(" \n==== Reparticion Finalizada === \n\n");
             // levantar pedidos
-            Console.Write("\n lavantando pedidos en la tienda" + names[idxs[i]]);
-            for (int j = 0; j < productos.Length; j++)
+            string toPrint = "\n lavantando pedidos en la tienda" + names[idxs[i]]+ " ===== " + getCurrentDateTime();
+
+            Console.Write(toPrint);
+            for (int j = 0; j < productos.Count; j++)
             {
                 Console.Write("\n cuantos productos de " + productos[j] +  " quiere?\n");
                 int ans = int.Parse(Console.ReadLine());
-                pedidos[idxs[i],j] = ans;
+                pedidos[idxs[i]][j] = ans;
 
             }
+
             Console.Clear();
         }
 
     }
 
-
+    static string getCurrentDateTime()
+    {
+        DateTime now = DateTime.Now;
+        return now.ToString("F");
+    }
 
 
     static void IniciarSimulacion()
     {
-        Console.Write("Iniciando la simulacion de productos");
+        string toPrint = "Iniciando la simulacion de productos";
+
+        Console.Write(toPrint);
+        wholeDatabase.EscribirEnBitacora(toPrint + "====="  + getCurrentDateTime());
         // este elemento es para guardar la maxima cantidad de camiones que pueden salir a repartir
         int tempCamiones = totalCamiones;
         // este arreglo es para llevar la cantidad que camiones que van a salir de cada tipo
-        int[] camionesCantidad = new int[camiones.Length];
+        int[] camionesCantidad = new int[camiones.Count];
 
         // este arreglo es para incluir la cantidad de elementos 
         // en todos los camiones que van a salir a repartir
-        int[] elementosARepartir = new int[camiones.Length];
+        int[] elementosARepartir = new int[camiones.Count];
 
-        for(int i=0;i<camiones.Length;i++)
+        for(int i=0;i<camiones.Count;i++)
         {
-            Console.Write("\nLos Camiones del tipo " + camiones[i] + "\ntienen capacidad para almacenar" + camionesCapacidad[i] + "elementos\n");
+            Console.Write("\nLos Camiones del tipo " + camiones[i] + "\ntienen capacidad para almacenar " + camionesCapacidad[i] + " elementos\n");
             Console.Write("\ncuantos camiones del tipo: " + camiones[i] + " max( " + totalCamiones + ")?\n");
             camionesCantidad[i] = int.Parse(Console.ReadLine());
-            totalCamiones -= camionesCantidad[i];
+            tempCamiones -= camionesCantidad[i];
 
-            elementosARepartir[i] = camionesCantidad[i] * camionesCapacidad[i];
+            elementosARepartir[i] = camionesCantidad[i] * (int)camionesCapacidad[i];
         }
 
 
         int[] idxs = sortIndeces(pedidos);
-        for (int i = 0; i < names.Length; i++)
+        for (int i = 0; i < names.Count; i++)
         {
             //repartir productos
             Console.Write("\n repartiendo en la tienda " + names[idxs[i]] + "\n");
-            for (int j = 0; j < productos.Length; j++)
+            for (int j = 0; j < productos.Count; j++)
             {
-                elementosARepartir[j] -= pedidos[idxs[i], j];
+                elementosARepartir[j] -= (int)pedidos[idxs[i]][j];
                 // esta es la traba para seguir obteniendo los camiones
                 while(elementosARepartir[j] < 0)
                 {
@@ -126,50 +144,95 @@ class Program
                     string answer = Console.ReadLine();
                     if(answer == "yes")
                     {
-                        elementosARepartir[j] += camionesCapacidad[j];
+                        elementosARepartir[j] += (int)camionesCapacidad[j];
+
+                        toPrint = "se agrego un camion repartidor de " + productos[j] + getCurrentDateTime();
+
+                        Console.Write(toPrint);
+                        wholeDatabase.EscribirEnBitacora(toPrint);
+
+
                     }
                 }
-                Console.Write("\nProducto " + productos[j] + " -- " + pedidos[idxs[i], j] + "\n");
+                Console.Write("\nProducto " + productos[j] + " -- " + pedidos[idxs[i]][j] + "\n");
             }
-            Console.Write(" \n==== Reparticion Finalizada === \n\n");
-            // levantar pedidos
-            Console.Write("\n lavantando pedidos en la tienda" + names[idxs[i]]);
-            for (int j = 0; j < productos.Length; j++)
-            {
-                Console.Write("\n cuantos productos de " + productos[j] + " quiere?\n");
-            }
+            Console.Write(" \n==== Simulacion de Reparticion Finalizada === \n\n");
+
+            toPrint = "\n==== Simulacion de Reparticion Finalizada === " + getCurrentDateTime()  ;
+
+            Console.Write(toPrint);
+            wholeDatabase.EscribirEnBitacora(toPrint);
+
+
+
+            Console.Write("simulacion exitosa");
+            string tempAns = Console.ReadLine();
+
             Console.Clear();
         }
     }
     static void FirstMenu()
     {
         Console.Write("==== Ingresa la accion que deseas ========\n\n");
-        Console.Write("1) iniciar simulacion de reparticion\n");
+        Console.Write("1) mostrar bitacora\n");
         Console.Write("2) Iniciar reparticion de productos\n");
+        Console.Write("3) salir\n");
+    }
+    static void InitVariables(CLass1 data_base)
+    {
+        //names = (String[])data_base.names;
+        
+        numCustomers = data_base.numCustomers;
+        pedidos = data_base.pedidos;
+        productos = data_base.productos;
+        camiones = data_base.camiones;
+        camionesCapacidad = data_base.camionesCapacidad;
+        totalCamiones = data_base.totalCamiones;
+        unidadesCamiones = data_base.unidadesCamiones;
+        names = data_base.names;
+        
+        
+    }
+    static void LoadDatabase()
+    {
+        if(databaseLoaded == false)
+        {
+            wholeDatabase = new CLass1();
+            InitVariables(wholeDatabase);
+            Console.Write("\n ==== imported the class succesfully ==== \n");
+            Console.Write("\n ======================================== \n");
+            databaseLoaded = true;
+        }
+    }
+    static void mostrarBitacora()
+    {
+        wholeDatabase.leerBitacora();
     }
     static void Main()
     {
         string option = "";
         int daysCount = 0;
-        CLass1 data_base =  new CLass1();
-
-        Console.Write("imported the class");
-        Console.Write("names: " + data_base.names);
-
-
         while (option == "" || option == "continue")
         {
             daysCount++;
             Console.Write("\nday:" + daysCount + "\n");
             FirstMenu();
             int x = int.Parse(Console.ReadLine());
+
             if (x == 1)
             {
+                LoadDatabase();
+                mostrarBitacora();
                 Console.Clear();
-                IniciarSimulacion();
+            }
+            else if(x == 3)
+            {
+                option = "exit";
             }
             else
             {
+                LoadDatabase();
+                IniciarSimulacion();
                 Console.Clear();
                 IniciarReparticion();
             }
